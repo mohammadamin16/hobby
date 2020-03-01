9eba9f88778010456ff3d341ed2f3298ebb1dc2e
Amin <toutounchi.ma@gmail.com>
2020-03-01 20:35:17 +0330

    Add Accounts App

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-    def create_user(self,username,name, password=""):
+    """
+    Creates and saves a User with the given email, date of
+    birth and password.
+    """
+    def create_user(self,username,name ,password=""):
         username = username
         password = password
         name     = name
@@ -12,19 +16,57 @@ class UserManager(BaseUserManager):
             password=password,
             name=name
         )
+        user.set_password(password)
+        user.save()
+        return user
 
 
-
+    def create_superuser(self, username, name, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            username=username,
+            password=password,
+            name=name,
+        )
+        user.is_admin = True
+        user.save()
+        return user
 
 
 class User(AbstractBaseUser):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
     name     = models.CharField(max_length=100)
 
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username', 'password']
+    REQUIRED_FIELDS = ['password', 'name']
+
+    objects = UserManager()
 
 
+    def __str__(self):
+        return self.username
 
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
--- hobby/settings.py
+++ hobby/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'films'
+    'films',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'accounts.User'
