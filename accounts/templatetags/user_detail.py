1accc618ef1a14bc108f163a9f06da17949b8c5b
Amin <toutounchi.ma@gmail.com>
2020-03-04 18:40:51 +0330

    Add msg system

--- accounts/templatetags/user_detail.py
+++ accounts/templatetags/user_detail.py
@@ -11,4 +11,15 @@ def list_of_movies(watched_films):
     return ", \n".join(titles)
 
 
+def alert_info(text):
+    ALERT = """
+    <div class="alert alert-info alert-dismissible fade show">
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+    {}
+    </div>
+    """.format(text)
+    return ALERT
+
+
 register.filter('list_of_movies', list_of_movies)
+register.filter('alert_info', alert_info)
