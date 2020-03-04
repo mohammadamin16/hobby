24f74643cd8d5e5ea2786a5f8f93a71c2aceb9dd
Amin <toutounchi.ma@gmail.com>
2020-03-04 17:25:38 +0330

    Create static files

--- hobby/settings.py
+++ hobby/settings.py
@@ -122,4 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/amin/PycharmProjects/hobby/static/files'
 AUTH_USER_MODEL = 'accounts.User'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
